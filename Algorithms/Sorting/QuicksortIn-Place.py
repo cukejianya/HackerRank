244af5a7708e71c51f75ca1b15c0692094aea0b2
Chinedum Mac <cukejianya@gmail.com>
2015-10-01 11:28:59 -0400

    Finish QuicksortIn-Place.py. Need to test.

--- Algorithms/Sorting/QuicksortIn-Place.py
+++ Algorithms/Sorting/QuicksortIn-Place.py
@@ -1,22 +1,30 @@
 def quicksort(ar, front, end):
-    pivot = ar[len(ar)-1]
-    bound_idx = 0;
-    for idx range(front, end):
+    # print end
+    # print ar
+    pivot = ar[end-1]
+    # print 'pivot', pivot
+    # print 'range', range(front, end)
+    bound_idx = front;
+    for idx in range(front, end):
         elm = ar[idx]
         if elm <= pivot:
             bigger_elm = ar[bound_idx]
             ar[bound_idx] = elm
             ar[idx] = bigger_elm
             bound_idx+=1
-     print "Before", ar
-     if (end-front) < 2:
-         return ar
+    # print "Before", ar
+    if (end-front) < 2:
+        # print "stop:...", ar
+        return ar
 
-    print " ".join([str(x) for x in ar)
+    print " ".join([str(x) for x in ar])
     left = [front, bound_idx-1]
+    # print "left", left
     ar = quicksort(ar, left[0], left[1])
     right = [bound_idx, end]
+    # print "right", right
     ar = quicksort(ar, right[0], right[1])
+    return ar
 
 
 
@@ -43,4 +51,4 @@ def quicksort(ar, front, end):
 n = int(raw_input())
 ar = [int(x) for x in raw_input().split()]
 
-quicksort(ar, [], [])
+quicksort(ar, 0, n)
