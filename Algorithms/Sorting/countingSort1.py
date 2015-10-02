e3d860ab0c3cc35ec954a20513cde4f2029862bd
Chinedum Mac <cukejianya@gmail.com>
2015-10-02 01:10:40 -0400

    Add and Finish countingSort1.py

---
+++ Algorithms/Sorting/countingSort1.py
@@ -0,0 +1,10 @@
+def countSort(ar, n):
+    ret_ar = [0 for x in range(100)]
+    for x in ar:
+        ret_ar[x]+=1
+    return " ".join([str(x) for x in ret_ar])
+
+n = int(raw_input())
+ar = [int(x) for x in raw_input().split()]
+
+print countSort(ar, n)
