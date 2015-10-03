de0e99d7ef9c5206eef48733610004edb9ce4594
Chinedum Mac <cukejianya@gmail.com>
2015-10-03 18:15:07 -0400

    Finish theFullCountingSort.py

--- Algorithms/Sorting/theFullCountingSort.py
+++ Algorithms/Sorting/theFullCountingSort.py
@@ -1,16 +1,28 @@
-def countSort(ar):
+def countSort(ar_pos, ar_str):
     pos_ar = [0 for x in range(100)]
-    for x in ar:
-        pos_ar = [y+1 if x[0]<=idx else y for idx, y in enumerate(pos_ar)]
+    for x in ar_pos:
+        pos_ar = [y+1 if x<=idx else y for idx, y in enumerate(pos_ar)]
+    # print pos_ar
+    ret_ar = []
+    y = 0
     for idx, x in enumerate(pos_ar):
-        for y in ar:
-            idx = y[1]
-            elm = y[0]
+        for turns in range(y, x):
+            jdx = ar_pos.index(idx)
+            print 'jdx', jdx
+            print ar_pos.pop(jdx)
+            ret_ar.append(ar_str.pop(jdx))
+        if x == len(ar):
+            break
+        y = x
 
-
-    return " ".join([str(x) for x in ret_ar])
+    return " ".join(ret_ar)
 
 n = int(raw_input())
-ar = [int(raw_input().split() for x in range(n)]
+ar = [raw_input().split() for x in range(n)]
 
-print countSort(ar)
+ar_pos = [int(x[0]) for x in ar]
+ar_str = ['-' if idx < n/2 else x[1] for idx, x in enumerate(ar)]
+# print ar_pos
+# print ar_str
+
+print countSort(ar_pos, ar_str)
