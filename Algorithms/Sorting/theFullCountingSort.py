71b81589a69d3e6df6f756690af270fcc710c317
Chinedum Mac <cukejianya@gmail.com>
2015-10-04 19:14:59 -0400

    Fix code. Make more efficient

--- Algorithms/Sorting/theFullCountingSort.py
+++ Algorithms/Sorting/theFullCountingSort.py
@@ -1,28 +1,19 @@
 def countSort(ar_pos, ar_str):
-    pos_ar = [0 for x in range(100)]
-    for x in ar_pos:
-        pos_ar = [y+1 if x<=idx else y for idx, y in enumerate(pos_ar)]
-    # print pos_ar
-    ret_ar = []
-    y = 0
-    for idx, x in enumerate(pos_ar):
-        for turns in range(y, x):
-            jdx = ar_pos.index(idx)
-            print 'jdx', jdx
-            print ar_pos.pop(jdx)
-            ret_ar.append(ar_str.pop(jdx))
-        if x == len(ar):
-            break
-        y = x
-
+    ret_ar = ['' for x in range(100)]
+    for idx, x in enumerate(ar_pos):
+        ret_ar[x] = ar_str[idx] if not ret_ar[x] else ret_ar[x] + ar_str[idx]
     return " ".join(ret_ar)
 
 n = int(raw_input())
-ar = [raw_input().split() for x in range(n)]
+ar_pos = []
+ar_str = []
 
-ar_pos = [int(x[0]) for x in ar]
-ar_str = ['-' if idx < n/2 else x[1] for idx, x in enumerate(ar)]
-# print ar_pos
-# print ar_str
+for turn in range(n):
+    io_ar = raw_input().split()
+    ar_pos.append(int(io_ar[0]))
+    if turn < n/2:
+        ar_str.append('-')
+    else:
+        ar_str.append(io_ar[1])
 
 print countSort(ar_pos, ar_str)
