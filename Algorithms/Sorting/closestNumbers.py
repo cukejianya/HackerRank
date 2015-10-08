1a09385ad9adf056a6526ed5c01dab08fe8b9a94
Chinedum Mac <cukejianya@gmail.com>
2015-10-08 00:56:40 -0400

    Make more efficient

--- Algorithms/Sorting/closestNumbers.py
+++ Algorithms/Sorting/closestNumbers.py
@@ -21,22 +21,22 @@ def quicksort(ar, front, end):
 def closest_numbers(ar):
     ret_ar = []
     x = ar[0] - ar[1]
-
     for idx, elm_x in enumerate(ar[:len(ar)-1]):
-        for elm_y in ar[idx+1:]:
-            num = abs(elm_x - elm_y)
-            if num <= x:
-                if num < x:
-                    ret_ar = []
-                    x = num
-                ret_ar+=[elm_x, elm_y]
+        elm_y = ar[idx+1]
+        num = abs(elm_x - elm_y)
+        if num <= x:
+            if num < x:
+                ret_ar = []
+                x = num
+            ret_ar+=[elm_x, elm_y]
 
     return ret_ar
 
 n = int(raw_input());
 ar = [int(x) for x in raw_input().split()];
-ar = closest_numbers(ar)
-n = len(ar)
 ar = quicksort(ar, 0, n)
+ar = closest_numbers(ar)
+
+
 
 print " ".join([str(x) for x in ar])
