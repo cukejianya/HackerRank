3963f9154e8ef3bb87e76f45af1cb6f05735f81c
Chinedum Mac <cukejianya@gmail.com>
2015-10-08 00:45:33 -0400

    Add sorting. Need to make algo more effecient

--- Algorithms/Sorting/closestNumbers.py
+++ Algorithms/Sorting/closestNumbers.py
@@ -1,6 +1,27 @@
+def quicksort(ar, front, end):
+    pivot = ar[end-1]
+    bound_idx = front;
+    for idx in range(front, end):
+        elm = ar[idx]
+        if elm <= pivot:
+            bigger_elm = ar[bound_idx]
+            ar[bound_idx] = elm
+            ar[idx] = bigger_elm
+            bound_idx+=1
+    if (end-front) < 2:
+        return ar
+
+    left = [front, bound_idx-1]
+    ar = quicksort(ar, left[0], left[1])
+    right = [bound_idx, end]
+    ar = quicksort(ar, right[0], right[1])
+
+    return ar
+
 def closest_numbers(ar):
     ret_ar = []
     x = ar[0] - ar[1]
+
     for idx, elm_x in enumerate(ar[:len(ar)-1]):
         for elm_y in ar[idx+1:]:
             num = abs(elm_x - elm_y)
@@ -10,8 +31,12 @@ def closest_numbers(ar):
                     x = num
                 ret_ar+=[elm_x, elm_y]
 
+    return ret_ar
 
-    return " ".join([str(x) for x in ret_ar])
-s = int(raw_input());
+n = int(raw_input());
 ar = [int(x) for x in raw_input().split()];
-print closest_numbers(ar)
+ar = closest_numbers(ar)
+n = len(ar)
+ar = quicksort(ar, 0, n)
+
+print " ".join([str(x) for x in ar])
