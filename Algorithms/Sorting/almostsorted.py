edbb74292724dbb7f7df08bed2f450bfd95e5e4e
Chinedum Ukejianya <cukejianya@gmail.com>
2015-10-21 03:09:18 +0000

    Make changes to almostSorted

--- Algorithms/Sorting/almostsorted.py
+++ Algorithms/Sorting/almostsorted.py
@@ -3,6 +3,7 @@ def almostSorted(ar):
     reverse = 0
     reverse_ar = []
     swap_idx = []
+    swap_ar = []
     for idx, elm in enumerate(ar):
         other = ar[idx+1]
         if elm > other:
@@ -14,10 +15,12 @@ def almostSorted(ar):
             if swap == 0:
                 swap_idx = [idx,idx+1]
                 swap += 1
+            else:
+                swap;
         else:
             if reverse > 0:
                 reverse_ar.append(elm)
-
+                
 
 
 n = raw_input()
