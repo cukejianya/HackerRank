833c323a408358b8e8283a099da691f0e41d24f0
Chinedum <cukejianya@gmail.com>
2015-10-23 14:54:56 -0400

    Redo logic

--- Algorithms/Sorting/almostsorted.py
+++ Algorithms/Sorting/almostsorted.py
@@ -1,62 +1,47 @@
 def almostSorted(ar):
-    swap = 0
     reverse = 0
-    reverse_ar = []
-    swap_idx = []
-    swap_ar = []
+    swap = 0
+    swap_ar = [0, len(ar)-1]
+    reverse_ar = [0, 0]
     for idx, elm in enumerate(ar):
-        print "swap", swap_ar
-        print "reverse", reverse_ar
-        print idx
-        if idx < len(ar) - 1:
-            other = ar[idx+1]
-        else:
-            other = elm + 1
-        if elm > other:
-            if reverse_ar and reverse == 1:
-                reverse_ar.append(other)
+        if idx == 0:
+            base = elm
+            continue
+        # print idx, elm
+        prev = ar[idx - 1]
+        if  prev > elm:
+            if not swap:
+                swap_ar = [idx, idx+1]
             else:
-                if reverse == 0:
-                    reverse_ar.append(elm)
-                    reverse_ar.append(other)
-                else:
-                    reverse = []
-                reverse += 1
-            if swap == 0:
-                swap_idx = [idx,idx+1]
-                swap += 1
-            elif swap == 1:
-                swap += 1
-                switch = ar[swap_idx[0]]
-                switch_next = ar[swap_idx[1]]
-                if other < switch_next:
-                    swap_ar = [switch, other]
-                else:
-                    swap_ar = []
-            else:
-                swap += 1
-                swap_ar = []
+                swap_ar[1] = idx+1
+            if not reverse:
+                reverse_ar = [idx - 1]
+            swap += 1
+            reverse += 1
         else:
-            if reverse > 1:
-                reverse_ar = []
+            if reverse and len(reverse_ar) == 1:
+                reverse_ar.append(idx-1)
+        #print swap_ar, swap
+    if len(reverse_ar) == 1:
+        reverse_ar.append(len(ar)-1)
+    #print reverse
+    #print reverse_ar
 
-    return [reverse_ar, swap_ar]
-
+    if swap and swap < 3 and (swap > 1 or len(ar) == 2):
+        return [True, "swap", " ".join(str(x) for x in swap_ar)]
+    elif reverse and reverse  == reverse_ar[1] - reverse_ar[0] \
+    and ar[reverse_ar[1]] > ar[reverse_ar[0] - 1]:
+        return [True, "reverse", " ".join(str(ar[x]) for x in reverse_ar[::-1])]
+    else:
+        return [False]
 n = raw_input()
 ar = [int(x) for x in raw_input().split()]
 
 ret_ar = almostSorted(ar)
 #print ret_ar
 
-if ret_ar[1]:
+if ret_ar[0]:
     print "yes"
-    print "swap"," ".join(str(x) for x in ret_ar[1])
-elif ret_ar[0]:
-    print "yes"
-    if len(ret_ar[0]) == 2:
-        print "swap",
-    else:
-        print "reverse",
-    print ret_ar[0][::-1][0], ret_ar[0][0]
+    print ret_ar[1], ret_ar[2]
 else:
     print "no"
