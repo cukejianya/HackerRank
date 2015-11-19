b38fd88add3a2ea940a2e399e7fe39fb1e604914
Chinedum <cukejianya@gmail.com>
2015-11-18 23:12:31 -0500

    Update almostsorted.py. Made it worst

--- Algorithms/Sorting/almostsorted.py
+++ Algorithms/Sorting/almostsorted.py
@@ -1,39 +1,32 @@
 def almostSorted(ar):
-    reverse = 0
-    swap = 0
-    swap_ar = [0, len(ar)-1]
-    reverse_ar = [0, 0]
+    mini = 0
+    maxi = 0
+    ar = [-1]+ar+[1000001]
+    n = len(ar)
     for idx, elm in enumerate(ar):
-        if idx == 0:
-            base = elm
+        #print idx, elm
+        if idx == 0 or idx == n - 1:
             continue
-        # print idx, elm
         prev = ar[idx - 1]
-        if  prev > elm:
-            if not swap:
-                swap_ar = [idx, idx+1]
-            else:
-                swap_ar[1] = idx+1
-            if not reverse:
-                reverse_ar = [idx - 1]
-            swap += 1
-            reverse += 1
-        else:
-            if reverse and len(reverse_ar) == 1:
-                reverse_ar.append(idx-1)
-        #print swap_ar, swap
-    if len(reverse_ar) == 1:
-        reverse_ar.append(len(ar)-1)
-    #print reverse
-    #print reverse_ar
+        nxt = ar[idx + 1]
+        if elm > prev and elm > nxt:
+            idx_ar = [idx, 0]
+            maxi += 1
+        if elm < prev and elm < nxt:
+            idx_ar[1] = idx
+            mini += 1
+    validate = (ar[idx_ar[1]+1] > ar[idx_ar[0]]);
+    edgeCaseSwap = idx_ar[1]-idx_ar[0] == 1;
 
-    if swap and swap < 3 and (swap > 1 or len(ar) == 2):
-        return [True, "swap", " ".join(str(x) for x in swap_ar)]
-    elif reverse and reverse  == reverse_ar[1] - reverse_ar[0] \
-    and ar[reverse_ar[1]] > ar[reverse_ar[0] - 1]:
-        return [True, "reverse", " ".join(str(ar[x]) for x in reverse_ar[::-1])]
+    if maxi == 0 and mini == 0:
+        return [True, False]
+    elif maxi == 1 and (mini == 2 or (validate and edgeCaseSwap)):
+        return [True, "swap", " ".join(str(x) for x in idx_ar)]
+    elif maxi == 1 and mini == 1 and validate:
+        return [True, "reverse", " ".join(str(ar[x]) for x in idx_ar[::-1])]
     else:
         return [False]
+
 n = raw_input()
 ar = [int(x) for x in raw_input().split()]
 
@@ -42,6 +35,7 @@ ret_ar = almostSorted(ar)
 
 if ret_ar[0]:
     print "yes"
-    print ret_ar[1], ret_ar[2]
+    if ret_ar[1]:
+        print ret_ar[1], ret_ar[2]
 else:
     print "no"
