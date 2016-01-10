15e4ff6ed9e13f2996c53ca5250c867c37b5ae93
Chinedum <cukejianya@gmail.com>
2016-01-09 23:41:43 -0500

    Add comparing substring function

--- Algorithms/Strings/commonChild.py
+++ Algorithms/Strings/commonChild.py
@@ -8,10 +8,23 @@ def check_similar_char(main, checkin):
             new.append(char)
     return new
 
+def substring_len(sub_first, second):
+    new = []
+    for char in sub_first:
+        if char in second:
+            new.append(char)
+            idx = second.index(char)
+            second = second[idx+1::]
+        if not second:
+            break;
+    print new
+    return len(new)
+
 def common_child(first, second):
     max_child = 0
     first = check_similar_char(first, second)
     second = check_similar_char(second, first)
+    for 
     print "".join(first), "".join(second)
 
 common_child(first_word, second_word)
