50cdde6b94e3541145f46ccd6f0838e49dd6d244
Chinedum Ukejianya <cukejianya@gmail.com>
2016-08-19 15:59:46 -0400

    Asearch exercise

---
+++ AI/Asearch/pacman_dfs.py
@@ -0,0 +1,12 @@
+pac_pos = setPos(raw_input());
+food_pos = setPos(raw_input());
+grid_bounds = setPos(raw_input());
+
+def setPos(str):
+    arr = map(int, str.split(" "))
+    p = {}
+    p.y = arr[0]
+    p.x = arr[1]
+    return p
+
+for 
