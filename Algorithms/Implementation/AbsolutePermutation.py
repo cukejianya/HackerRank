5333e0f865c87ffe68a7c7d5b5ea669cb7431553
Chinedum Ukejianya <cukejianya@gmail.com>
2016-10-12 23:51:30 -0400

    add check hash method

--- Algorithms/Implementation/AbsolutePermutation.py
+++ Algorithms/Implementation/AbsolutePermutation.py
@@ -1,25 +1,41 @@
+import math
+
 def find_abs_permutation(n, k):
     set_dict = {}
     for i in range(1, n+1):
-        higher_idx = k + i
-        lower_idx = k - i
-        if set_pos(higher_idx, set_dict, n)
-            or set_pos(lower_idx, set_dict, n):
+        higher_idx = i + k
+        lower_idx = i - k
+        set_pos(higher_idx, set_dict, n, i)
+        set_pos(lower_idx, set_dict, n, i)
+        if len(set_dict) not n:
             return -1
-    d = [set_dict.get(x) for x in range(1, n+1)]
-    return d
+    return set_dict
 
-def set_pos(pos, hash, n):
+def set_pos(pos, hash, n, i):
     if (pos < 1 or pos > n):
-        return true
-    if pos not in set_dict:
-        set_dict[pos] = [i]
-        return false
-    elif len(set_dict[pos]) is 1:
-        set_dict.append(pos)
-        return false
+        return
+    if pos not in hash:
+        hash[pos] = [i]
     else:
-        return true
+        hash[pos].append(pos)
+
+def check_hash(hash, n):
+    seen_nums = set([])
+    for i in range(1, math.ceil(n/float(2))+1):
+        front_list = set_dict[i]
+        end_list = set_dict[n + 1 - i]
+        if len(front_list) is 1 front_list[0] not in seen_nums:
+            seen_nums.add(front_list)
+        if len(end_list) is 1:
+            if end_list[n + 1 - i][0] not in seen_nums:
+                seen_nums.add(end_list[0])
+            else:
+                return -1
+        else:
+            
+
+
+
 
 T = int(raw_input());
 for x in range(T):
